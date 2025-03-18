15c45845f44aae31e5c5190bf0d504e75e850b8f
254-Ben <obenard300@gmail.com>
2025-03-18 12:36:58 +0300

    week1 assignment

---
+++ calc.py
@@ -0,0 +1,18 @@
+User = input("Enter an operator (+, -, *, /):")
+num1 = float(input("Enter 1st number:"))
+num2 = float(input("Enter 2nd number:"))
+
+if User == "+":
+    result = num1 + num2
+    print(round(result, 2))
+elif User == "-":
+    result = num1 - num2
+    print(round(result, 2))
+elif User == "*":
+    result = num1 * num2
+    print(round(result, 2))
+elif User == "/":
+    result = num1 / num2
+    print(round(result, 2))
+else:
+    print("Invalid operator")
