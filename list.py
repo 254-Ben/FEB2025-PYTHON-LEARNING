3206b1a22673a09c89c45679c97bedfe1f5725f9
254-Ben <obenard300@gmail.com>
2025-03-25 17:38:24 +0300

    python week 2 assignment

---
+++ list.py
@@ -0,0 +1,23 @@
+# empty list
+my_list = []
+
+# using append to add elements to list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# inserting valus 15 at 2nd position
+my_list.insert(1, 15)
+
+# extend my_list with another list [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Remove last item from my_list
+my_list.pop()
+
+# sort my_list in ascending order
+my_list.sort()
+
+# get the index of value 30 in my_list
+index = my_list.index(30)
